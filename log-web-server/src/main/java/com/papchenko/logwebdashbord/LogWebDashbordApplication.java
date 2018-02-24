fe8b59cbb657ce2532072c19ce40e382bf7279c2
papchenko <rocketmannot@gmail.com>
2018-02-24 13:51:55 +0200

    initial commit

---
+++ log-web-server/src/main/java/com/papchenko/logwebdashbord/LogWebDashbordApplication.java
@@ -0,0 +1,12 @@
+package com.papchenko.logwebdashbord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogWebDashbordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogWebDashbordApplication.class, args);
+	}
+}
---
+++ log-web-server/src/test/java/com/papchenko/logwebdashbord/LogWebDashbordApplicationTests.java
@@ -0,0 +1,16 @@
+package com.papchenko.logwebdashbord;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LogWebDashbordApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
